3883ddc6bdfa8dafac8d58b8b0a71e50031dccf4
Alex <agordon1123@gmail.com>
2019-09-19 12:46:23 -0400

    created three difference async server tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -10,7 +10,23 @@ describe('server.js', () => {
              });
             // assert that we get an http status code 200
         });
+        it("should return { api: 'up' }", async () => {
+            const res = await request(server).get('/');
+            expect(res.body.api).toBe('up');
+        });
+
+        // this example uses done() to declare as async
+        it('returns JSON', done => {
+            request(server)
+            .get('/')
+            .then(res => {
+                // assert that we get an http status code 200
+                expect(res.type).toMatch(/json/i);
+                done();
+            });
+        });
     });
 });
 
 // one GOTCHA is that we must return the request with asyncronous calls or it will reach the bottom without faiing before the promise is returned
+    // or we can use the async keyword or the done() fn
